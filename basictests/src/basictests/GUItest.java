1712dabcc69e3d7e1ff3a3d7fcbc6217b93e46eb
Indiana Davis <indianadavis1995@gmail.com>
2017-11-25 21:46:10 -0800

    Trial 1

---
+++ basictests/src/basictests/GUItest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package basictests;
+
+import javax.swing.JOptionPane;
+/**
+ * @author indyd_000
+ *
+ */
+public class GUItest {
+
+	/**
+	 * @param theArgs unused
+	 */
+	public static void main(String[] theArgs) {
+		JOptionPane.showMessageDialog( null, "Hello World!" );
+	   }
+	}
