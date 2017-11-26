92dcb3036e2fe3b6ba180a2ba2b801d7677a2465
Indiana Davis <indianadavis1995@gmail.com>
2017-11-26 12:58:52 -0800

    a

---
+++ davisind-orientation/src/Hello.java
@@ -0,0 +1,25 @@
+/*
+ * TCSS 305 - Orientation Homework
+ */
+
+/**
+  * @author indyd_000
+  * @version 1
+  */
+public final class Hello {
+    /**
+      * 
+      */
+    private Hello() {
+        
+    }
+
+    /**
+      * @param theArgs not used.
+      */
+    public static void main(final String[] theArgs) {
+        System.out.println("Hello, World!");
+
+    }
+
+}
---
+++ davisind-orientation/src/Home.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author indyd_000
+ * @version 2
+ */
+public final class Home {
+    /**
+     * 
+     */
+    private Home() {
+        
+    }
+    /**
+     * @param theArgs is not used.
+     */
+    public static void main(final String[] theArgs) {
+        System.out.println("Hello, World!");
+    }
+
+}
